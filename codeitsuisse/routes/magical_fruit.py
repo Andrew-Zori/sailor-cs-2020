fb12bd4fab7684c335339d760b7ec1657a122dbe
Andrew ZHOU <zzhouba@connect.ust.hk>
2020-09-26 19:07:44 +0800

    Update magical_fruit.py

--- codeitsuisse/routes/magical_fruit.py
+++ codeitsuisse/routes/magical_fruit.py
@@ -9,17 +9,18 @@ from codeitsuisse import app
 logger = logging.getLogger(__name__)
 
 @app.route('/fruitbasket', methods=['POST'])
-def evaluateFruitbasket():
+def evaluatefruit():
     data = request.get_data()
-    data = json.loads(data)
-    logging.info("data sent for evaluation {}".format(data))
-    # inputValue = data.get("input");
+    data = json.loads(data.decode('utf-8'))
+    ans = 0
 
-    result = (guess_result(data["maApple"], data["maWatermelon"],data["maBanana"]))
+    weight = {"maApple": 60,
+    "maWatermelon": 60,
+    "maBanana": 60}
 
-    # result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return jsonify(result)
+    for key, value in data.items():
+        ans += weight[key] * value
+    return str(ans)
 
 def guess_result(ma, mb, mc):
     wa = 60
