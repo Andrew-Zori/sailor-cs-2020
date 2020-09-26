bbc760565b6c1bfc806001d58b673914e263b694
Andrew ZHOU <zzhouba@connect.ust.hk>
2020-09-26 19:17:38 +0800

    Update magical_fruit.py

--- codeitsuisse/routes/magical_fruit.py
+++ codeitsuisse/routes/magical_fruit.py
@@ -14,17 +14,14 @@ def evaluatefruit():
     data = json.loads(data.decode('utf-8'))
     ans = 0
 
-    weight = {"maApple": 60,
-    "maWatermelon": 60,
-    "maBanana": 60}
+    name_list = []
+    for key in data.keys():
+        name_list.append(key)
+
+    weight = {name_list[0]: 60,
+    name_list[1]: 60,
+    name_list[2]: 60}
 
     for key, value in data.items():
         ans += weight[key] * value
     return str(ans)
-
-def guess_result(ma, mb, mc):
-    wa = 60
-    wb = 60
-    wc = 60
-    return (int(ma) * wa + int(mb) * wb + int(mc) * wc)
-    # return (mb)
