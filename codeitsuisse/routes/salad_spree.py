b4a629e7fcdbba61951407b6c61b29cd11e04bb5
Andrew ZHOU <zzhouba@connect.ust.hk>
2020-09-26 11:27:49 +0800

    Update salad_spree.py

--- codeitsuisse/routes/salad_spree.py
+++ codeitsuisse/routes/salad_spree.py
@@ -23,28 +23,26 @@ def evaluateSalad_Spree():
 def find_salad(n, street_map):
     n = int(n)
     np_sm = np.array(street_map)
-    direction_list = [np_sm, np_sm.transpose()]
     find_one = False
     cheapest = 1000
-    for street_dir in direction_list:
-        for street in street_dir:
-            start = 0
-            end = n
-            while(end <= len(street)):
-                if np.where(np.isin(street[start: end],np.array(["X"])))[0].size != 0:
-                    no_salad_index = np.isin(street,np.array(["X"]))[-1]
-                    end += no_salad_index + 1
-                    start += no_salad_index + 1
-                else:
-                    to_sum = street[start:end].astype(np.int32)
-                    if find_one:
-                        if np.sum(to_sum)<cheapest:
-                            cheapest = np.sum(to_sum)
-                    else:
+    for street in np_sm:
+        start = 0
+        end = n
+        while(end <= len(street)):
+            if np.where(np.isin(street[start: end],np.array(["X"])))[0].size != 0:
+                no_salad_index = np.isin(street,np.array(["X"]))[-1]
+                end += no_salad_index + 1
+                start += no_salad_index + 1
+            else:
+                to_sum = street[start:end].astype(np.int32)
+                if find_one:
+                    if np.sum(to_sum)<cheapest:
                         cheapest = np.sum(to_sum)
-                        find_one = True
-                    end += 1
-                    start += 1
+                else:
+                    cheapest = np.sum(to_sum)
+                    find_one = True
+                end += 1
+                start += 1
     dic = {}
     
     if find_one:
