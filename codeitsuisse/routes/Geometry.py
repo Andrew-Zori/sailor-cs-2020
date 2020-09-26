c57a1b44f1cd44273bbb4d7a4197dd634d7978f4
Andrew ZHOU <zzhouba@connect.ust.hk>
2020-09-26 18:26:11 +0800

    Update Geometry.py

--- codeitsuisse/routes/Geometry.py
+++ codeitsuisse/routes/Geometry.py
@@ -19,20 +19,6 @@ def evaluateGeometry():
     # result = inputValue * inputValue
     logging.info("My result :{}".format(result))
     return jsonify(result)
-
-def intersection_detector(x1,y1,x2,y2, x3,y3,x4,y4):
-    u=(x3-x1)*(y2-y1)-(x2-x1)*(y3-y1)
-    v=(x4-x1)*(y2-y1)-(y4-y1)*(x2-x1)
-    w=(x1-x3)*(y4-y3)-(x4-x3)*(y1-y3)
-    z=(x2-x3)*(y4-y3)-(x4-x3)*(y2-y3)
-
-    if(u*v<=0 and w*z<=0 and\
-       min(x1,x2)<=max(x3,x4)and min(x3,x4)<=max(x1,x2)\
-       and min(y1,y2)<=max(y3,y4) and min(y3,y4)<=max(y1,y2)):
-        return True
-    else:
-        return False
-
 # dic_list {"x": , "y",}
 
 def result_func(shape_cords, line_cords): 
@@ -64,8 +50,18 @@ def result_func(shape_cords, line_cords):
             x_intersect = (c1*b2-c2*b1)/(a2*b1-a1*b2)
             y_intersect = (a2*c1-a1*c2)/(a1*b2-a2*b1)
 
+            if int(x_intersect) != x_intersect:
+                x_intersect = round(x_intersect,2)
+            else:
+                x_intersect = int(x_intersect)
+            if int(y_intersect) != y_intersect:
+                y_intersect = round(y_intersect,2)
+            else:
+                y_intersect = int(y_intersect)
+
             if x_intersect <= max(x1,x2) and x_intersect >= min(x1,x2)\
                and y_intersect <= max(y1,y2) and y_intersect >= min(y1,y2):
-                result.append({"x" : round(x_intersect,2),\
-                           "y" : round(y_intersect,2)})
+                
+                result.append({"x" : x_intersect,\
+                           "y" : y_intersect})
     return result
